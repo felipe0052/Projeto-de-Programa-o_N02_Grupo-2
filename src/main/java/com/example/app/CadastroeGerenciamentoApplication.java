249fd1f30f7585ccd5c5f9fa3f167cb970990e3e
André Felipe <felipebarreto.gly@gmail.com>
2025-10-13 20:37:26 -0300

    EstruturaInicial
    A primeira estrutura do projeto dividido por packages

---
+++ src/main/java/com/example/app/CadastroeGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroeGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroeGerenciamentoApplication.class, args);
+	}
+
+}
