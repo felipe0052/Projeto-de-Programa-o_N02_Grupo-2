7f67297c6b489accc9a7dc649a642e7f86489eff
GlytonN <160677791+GlytonN@users.noreply.github.com>
2025-11-03 16:21:09 -0300

    ReadMeOnly

--- src/main/java/com/example/app/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.app.repository;
-
-import com.example.app.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
