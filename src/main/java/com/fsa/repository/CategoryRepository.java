a8c04dd0166ef485e550234ad89bc7b83b3aa263
GlytonN <160677791+GlytonN@users.noreply.github.com>
2025-11-17 15:46:22 -0300

    vazio

--- src/main/java/com/fsa/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fsa.repository;
-
-import com.fsa.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    boolean existsByNomeIgnoreCase(String nome);
-}
-
